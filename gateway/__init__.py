4c19d4f1e87cda291d31dacd64149aab4d253da2
Renato Bianco <renato.bianco@hotmail.com>
2022-02-21 02:13:56 +0000

    set env var for Heroku

--- gateway/__init__.py
+++ gateway/__init__.py
@@ -7,9 +7,9 @@ import braintree
 gateway = braintree.BraintreeGateway(
     braintree.Configuration(
         braintree.Environment.Sandbox,
-        merchant_id=BT_MERCHANT_ID,
-        public_key=BT_PUBLIC_KEY,
-        private_key=BT_PRIVATE_KEY
+        merchant_id=os.getenv("BT_MERCHANT_ID"),
+        public_key=os.getenv("BT_PUBLIC_KEY"),
+        private_key=os.getenv("BT_PRIVATE_KEY")
     )
 )
 
